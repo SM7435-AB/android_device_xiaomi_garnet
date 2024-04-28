3f6579109c36fbfb7b240e5f2dbb9c1f141cafac
Elias Dimopoulos <linkdevel@protonmail.com>
2024-04-28 22:20:59 +0530

    garnet: Add WfdCommon to boot JARs for WFD
    Change-Id: Iee69115e681b15d0be0108ac395dd2db11be92be
    Signed-off-by: Elias Dimopoulos <linkdevel@protonmail.com>

--- device.mk
+++ device.mk
@@ -554,5 +554,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libwfdaac_vendor
 
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # Vendor
 $(call inherit-product, vendor/xiaomi/garnet/garnet-vendor.mk)
